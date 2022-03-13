2bffeb56bebab55e75a5471b5d692023eb55fb0a
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-03-13 14:58:18 +0300

    set up profile model

--- codeonapp/models.py
+++ codeonapp/models.py
@@ -31,3 +31,31 @@ class Profile(models.Model):
             return self.photo.url
         else:
             return "/static/images/default-profile.jpg"
+
+
+class Projects (models.Model):
+    title = models.CharField(max_length =40)
+    image = CloudinaryField('image')
+    description =  HTMLField()
+    profile = models.ForeignKey(Profile,null = True)
+    poster = models.ForeignKey(User,on_delete=models.CASCADE , null=True) 
+    post_date=models.DateTimeField(auto_now_add=True)
+    link = models.CharField(max_length =200)
+    
+    def __str__(self):
+        return self.title
+    
+    def save_projects(self):
+        self.save()
+        
+    def update_projects(self):
+        self.update()
+
+    def delete_projects(self):
+        self.delete() 
+                
+    @classmethod
+    def search_by_title(cls,search_term):
+        titles = cls.objects.filter(title__icontains=search_term)
+        return titles  
+    
