6a596f5051d93642ba5b7ea7f947ccb8f191706e
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-03-13 17:01:16 +0300

    project page view

--- codeonapp/views.py
+++ codeonapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Projects, Profile, Rating, User
+
 
 # Create your views here.
+def welcome(request):
+    projects = Projects.objects.all().order_by("post_date")
+    profile = Profile.objects.all()
+
+    return render(request, 'index.html' ,{'projects':projects}, {'profile':profile})
+
+
