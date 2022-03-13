42b3b96e9615880e4bd660f2b5bfd3d17525a0cc
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-03-13 15:41:59 +0300

    add new model forms

---
+++ codeonapp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Merch, Projects,Profile,Rating
+
+class NewProjectsForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        exclude = ['profile', 'post_date', 'poster', 'description']
+        
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['username']        
+       
+class NewRatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        exclude = ['profile','project','user','total','avg']  
+
+class NewMerchForm(forms.ModelForm):
+    class Meta:
+        model = Merch
+               
