ad8e59872fe79891011d44021d0755861a350868
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-03-13 15:13:33 +0300

    test profile model

--- codeonapp/tests.py
+++ codeonapp/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from . models import Projects, Profile, Rating
 
 # Create your tests here.
+
+# Profile model tests 
+      
+class ProfileTestClass(TestCase):        
+    
+        # Set up method
+    def setUp(self):
+        self.image= Profile(profile_pict = 'img.jpg', bio ='image', email = "ness@gmail.com",phone_number = '0785429922')
+        
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Profile))     
+
+        # Testing Save Method of Profile model
+    def test_save_method(self):
+        self.image.save_profile()
+        images = Profile.objects.all()
+        self.assertTrue(len(images) > 0)   
+        
+    # Testing  delete method of Profile model     
+    def test_delete(self):
+        self.image= Profile(profile_pict = 'img.jpg', bio ='image')
+        self.image.save_profile()
+        image = Profile.objects.filter(profile_pict = 'img.jpg').first()
+        delete = Profile.objects.filter(id = image.id).delete()
+        images = Profile.objects.all()
+        self.assertTrue(len(images) == 0)         
+
+    # Testing  update method of Profile model    
+    def test_update(self):
+        self.image.save_profile()
+        image = Profile.objects.filter(profile_pict = 'img.jpg').first()
+        update = Profile.objects.filter(id = image.id).update(profile_pict = 'cake.jpg')
+        updated = Profile.objects.filter(profile_pict = 'cake.jpg').first()
+        self.assertNotEqual(image.profile_pict, updated.profile_pict) 
