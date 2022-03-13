b981b14b07c46746ce82899bbfd928636dcbcc25
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-03-13 14:25:42 +0300

    initialize code on app

--- codeonproject/settings.py
+++ codeonproject/settings.py
@@ -48,10 +48,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'codeonapp',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
