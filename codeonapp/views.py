c7a0a787c894ccb2fd4c0c97b8e8ea7ae6df6bb5
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-03-13 17:02:10 +0300

    search function view

--- codeonapp/views.py
+++ codeonapp/views.py
@@ -11,4 +11,16 @@ def welcome(request):
 
     return render(request, 'index.html' ,{'projects':projects}, {'profile':profile})
 
+# search function
+def search_project(request):
 
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        searched_projects = Projects.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
